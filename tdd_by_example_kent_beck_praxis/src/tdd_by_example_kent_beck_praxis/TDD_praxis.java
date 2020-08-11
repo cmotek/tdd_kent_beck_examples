0017a1f116468de80ecf2980291004df1c4c87c9
cmotek <38844285+cmotek@users.noreply.github.com>
2020-08-10 19:39:51 -0700

    chaptwo-chapfour

--- tdd_by_example_kent_beck_praxis/src/tdd_by_example_kent_beck_praxis/TDD_praxis.java
+++ tdd_by_example_kent_beck_praxis/src/tdd_by_example_kent_beck_praxis/TDD_praxis.java
@@ -1,37 +1,43 @@
 package tdd_by_example_kent_beck_praxis;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TDD_praxis {
-
-	@Test
-	public void main() {
-		// TODO Auto-generated method stub
 		
 		class Dollar {
 			
-		int amount;
+		private int amount;
 			
 		Dollar(int amount){
 			this.amount= amount;
 		}
 		
 		
-		void times(int multiplier) {
-			amount *= multiplier;
+		Dollar times(int multiplier) {
+			return new Dollar(amount * multiplier);
+
 		}
 		
+		public boolean equals(Object object) {
+			Dollar dollar= (Dollar) object;
+			return amount == dollar.amount;
+		}
 		
+		}
+		
+		@Test
 		public void testMultiplication() {
 			Dollar five= new Dollar(5);
-			five.times(2);
-			assertEquals(10, five.amount);
-			
+			assertEquals(new Dollar(10), five.times(2));
+			assertEquals(new Dollar(15), five.times(3));			
 			}
 		
+		@Test
+		public void testEquality() {
+			assertTrue(new Dollar(5).equals(new Dollar(5)));
+			assertFalse(new Dollar(5).equals(new Dollar(6)));
 		}
-
-	}
+		
 }
